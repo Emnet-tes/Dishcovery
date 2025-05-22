96cf8d8f5d3149039dec266da62f473cb3472ad7
Emnet-tes <teshomeemnet255@gmail.com>
2025-05-22 06:35:41 +0300

    feat: setup layout

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true
+  },
+  modules: ['@nuxtjs/tailwindcss'],
+  experimental: {
+      scanPageMeta: 'after-resolve',
+      sharedPrerenderData: false,
+      compileTemplate: true,
+      resetAsyncDataToUndefined: true,
+      templateUtils: true,
+      relativeWatchPaths: true,
+      normalizeComponentNames: false,
+      spaLoadingTemplateLocation: 'within',
+      parseErrorData: false,
+      pendingWhenIdle: true,
+      alwaysRunFetchOnKeyChange: true,
+      defaults: {
+        useAsyncData: {
+          deep: true
+        }
+      }
+    },
+    features: {
+      inlineStyles: true
+    },
+    unhead: {
+      renderSSRHeadOptions: {
+        omitLineBreaks: false
+      }
+    }
+});
